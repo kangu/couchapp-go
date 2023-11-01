872ed7a6b5e5d45e8056191484cc94c5c0f389e8
Radu Stanciu <radu@kangu.ro>
2023-11-01 18:22:49 +0200

    update windows build script

--- build_scripts/windows.bat
+++ build_scripts/windows.bat
@@ -1,2 +1,7 @@
 cd ..
-GOOS=windows GOARCH=amd64 go build -o bin/couchapp-go.exe
+
+:: Run this if you want to build from OSX
+:: GOOS=windows GOARCH=amd64 go build -o bin/windows/couchapp-go.exe
+
+:: Run this if you build directly from Windows
+go build -o bin/windows/couchapp-go.exe
